6bc4040b5f2954687a039ed2257a426858499e4a
pongwit2550 <126218047+pongwit2550@users.noreply.github.com>
2024-11-20 23:01:48 +0700

    Delete public directory

--- public/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# ติดตั้ง Composer
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
-# ติดตั้ง PHP dependencies
-composer install
--- public/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// logout.php - ออกจากระบบ
-session_start();
-session_unset(); // ล้างตัวแปร session ทั้งหมด
-session_destroy(); // ทำลาย session
-header("Location: index.php");
-exit();
-?>
